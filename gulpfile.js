d28920fca850c20365de5af6b45aa032a716e19c
Gagan Brar <brargagandeepsingh87@gmail.com>
2018-05-17 13:48:05 -0500

    minified

--- gulpfile.js
+++ gulpfile.js
@@ -2,19 +2,15 @@ var gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
 
-gulp.task('autoprefixer', function () {
-    gulp.src(['./src/css/*.css'])
-        .pipe(autoprefixer({
-            browsers: ['last 10 versions'],
-            cascade: false
-        }))
-})
+gulp.task('css', function () {
+            gulp.src(['./src/css/*.css'])
+                .pipe(autoprefixer({
+                    browsers: ['last 10 versions'],
+                    cascade: false
+                })).pipe(cleanCSS({
+                    compatibility: 'ie8'
+                }))
 
+                .pipe(gulp.dest('./dist/css/'))
 
-gulp.task('minify', () => {
-    return gulp.src('./src/css/*.css')
-        .pipe(cleanCSS({
-            compatibility: 'ie8'
-        }))
-        .pipe(gulp.dest('./dist/css/*css'));
-});
+            });
