be9b94217de0659b47e0d9e26ee9e0724272cb4b
Gagan Brar <brargagandeepsingh87@gmail.com>
2018-05-17 13:13:54 -0500

    next commit

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,6 @@
 var gulp = require('gulp');
-
 const autoprefixer = require('gulp-autoprefixer');
-
+const cleanCSS = require('gulp-clean-css');
 
 gulp.task('autoprefixer', function () {
     gulp.src(['./src/css/*.css'])
@@ -10,3 +9,12 @@ gulp.task('autoprefixer', function () {
             cascade: false
         }))
 })
+
+
+gulp.task('minify', () => {
+    return gulp.src('./src/css/*.css')
+        .pipe(cleanCSS({
+            compatibility: 'ie8'
+        }))
+        .pipe(gulp.dest('./dist/css/*css'));
+});
