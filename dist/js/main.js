7d32edafab63fe68eedba7fce9f0887872244848
Gagan Brar <brargagandeepsingh87@gmail.com>
2018-05-17 14:27:08 -0500

    concat is done

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,1 +1,239 @@
-"use strict";var level=document.getElementById("level"),highest=document.getElementById("high"),Enemy=function(e,n,t){this.x=e,this.y=n,this.speed=t,this.sprite="images/enemy-bug.png"};Enemy.prototype.update=function(e){this.x=this.x+this.speed*level.textContent*e,550<this.x&&(this.x=-100),this.x+40>player.x&&player.x+40>this.x&&this.y+40>player.y&&player.y+40>this.y&&(player.x=200,player.y=460,level.textContent=1)},Enemy.prototype.render=function(){ctx.drawImage(Resources.get(this.sprite),this.x,this.y)};var Player=function(e,n){this.x=e,this.y=n,this.sprite="images/char-boy.png"};Player.prototype.update=function(){player.y<50&&(level.textContent++,level.textContent>highest.textContent&&highest.textContent++,document.getElementById("won").style.display="flex",player.x=200,player.y=460,won=!0)},Player.prototype.render=function(){ctx.drawImage(Resources.get(this.sprite),this.x,this.y)},Player.prototype.handleInput=function(e){document.getElementById("won").style.display="none","left"==e&&50<player.x?player.x-=100:"up"==e&&0<player.y?player.y-=75:"down"==e&&player.y<465?player.y+=75:"right"==e&&player.x<400&&(player.x+=100)};var enemy1=new Enemy(200,220,100),enemy2=new Enemy(100,140,200),enemy3=new Enemy(150,50,240),enemy4=new Enemy(80,295,50),enemy5=new Enemy(220,390,70),allEnemies=[enemy1,enemy2,enemy3,enemy4,enemy5],player=new Player(200,460);document.addEventListener("keyup",function(e){player.handleInput({37:"left",38:"up",39:"right",40:"down"}[e.keyCode])});var Engine=function(e){var t,n=e.document,a=e.window,o=n.createElement("canvas"),r=o.getContext("2d");function i(){var n,e=Date.now();n=(e-t)/1e3,allEnemies.forEach(function(e){e.update(n)}),player.update(),function(){var e,n,t=["images/water-block.png","images/stone-block.png","images/stone-block.png","images/stone-block.png","images/stone-block.png","images/stone-block.png","images/grass-block.png"];for(r.clearRect(0,0,o.width,o.height),e=0;e<7;e++)for(n=0;n<8;n++)r.drawImage(Resources.get(t[e]),101*n,83*e);allEnemies.forEach(function(e){e.render()}),player.render()}(),t=e,a.requestAnimationFrame(i)}o.width=505,o.height=707,n.body.appendChild(o),Resources.load(["images/stone-block.png","images/water-block.png","images/grass-block.png","images/enemy-bug.png","images/char-boy.png","images/Star.png"]),Resources.onReady(function(){t=Date.now(),i()}),e.ctx=r}(void 0);!function(){var t={},a=[];function n(e){if(t[e])return t[e];var n=new Image;n.onload=function(){t[e]=n,o()&&a.forEach(function(e){e()})},t[e]=!1,n.src=e}function o(){var e=!0;for(var n in t)t.hasOwnProperty(n)&&!t[n]&&(e=!1);return e}window.Resources={load:function(e){e instanceof Array?e.forEach(function(e){n(e)}):n(e)},get:function(e){return t[e]},onReady:function(e){a.push(e)},isReady:o}}();
+"use strict";
+
+var level = document.getElementById("level");
+var highest = document.getElementById("high");
+// constructor for the enemies
+var Enemy = function Enemy(x, y, speed) {
+
+    this.x = x;
+    this.y = y;
+    this.speed = speed;
+    this.sprite = 'images/enemy-bug.png';
+};
+//To increase the positionof enemies with respect to time.
+Enemy.prototype.update = function (dt) {
+    this.x = this.x + this.speed * level.textContent * dt;
+    if (this.x > 550) {
+        this.x = -100;
+    }
+    if (this.x + 40 > player.x && player.x + 40 > this.x && this.y + 40 > player.y && player.y + 40 > this.y) {
+        player.x = 200;
+        player.y = 460;
+        level.textContent = 1;
+    }
+};
+//to load particular images for the enemies .
+Enemy.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+//player constructor.
+var Player = function Player(x, y) {
+    this.x = x, this.y = y, this.sprite = 'images/char-boy.png';
+};
+// function to increase the levels , to check the highest level and to restart the game as the player reach the end point. 
+Player.prototype.update = function () {
+    if (player.y < 50) {
+        level.textContent++;
+        if (level.textContent > highest.textContent) {
+            highest.textContent++;
+        }
+        //Winning is the text displayed as the player reaches the final point.
+        var winning = document.getElementById("won");
+        winning.style.display = "flex";
+        player.x = 200;
+        player.y = 460;
+        won = true;
+    }
+};
+
+Player.prototype.render = function () {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+// THis function helps to use the keys for the movement of the player.
+Player.prototype.handleInput = function (direction) {
+    var winning = document.getElementById("won");
+
+    winning.style.display = "none";
+
+    if (direction == 'left' && player.x > 50) {
+        player.x -= 100;
+    } else if (direction == 'up' && player.y > 0) {
+        player.y -= 75;
+    } else if (direction == 'down' && player.y < 465) {
+        player.y += 75;
+    } else if (direction == 'right' && player.x < 400) {
+        player.x += 100;
+    }
+};
+// The enemies and players with their particular initial positions.
+var enemy1 = new Enemy(200, 220, 100);
+var enemy2 = new Enemy(100, 140, 200);
+var enemy3 = new Enemy(150, 50, 240);
+var enemy4 = new Enemy(80, 295, 50);
+var enemy5 = new Enemy(220, 390, 70);
+var allEnemies = [enemy1, enemy2, enemy3, enemy4, enemy5];
+var player = new Player(200, 460);
+
+document.addEventListener('keyup', function (e) {
+    var allowedKeys = {
+        37: 'left',
+        38: 'up',
+        39: 'right',
+        40: 'down'
+    };
+
+    player.handleInput(allowedKeys[e.keyCode]);
+});
+'use strict';
+
+var Engine = function (global) {
+
+    var doc = global.document,
+        win = global.window,
+        canvas = doc.createElement('canvas'),
+        ctx = canvas.getContext('2d'),
+        lastTime;
+
+    canvas.width = 505;
+    canvas.height = 707;
+    doc.body.appendChild(canvas);
+
+    function main() {
+
+        var now = Date.now(),
+            dt = (now - lastTime) / 1000.0;
+
+        update(dt);
+        render();
+
+        lastTime = now;
+
+        win.requestAnimationFrame(main);
+    }
+
+    function init() {
+        reset();
+        lastTime = Date.now();
+        main();
+    }
+
+    function update(dt) {
+        updateEntities(dt);
+    }
+
+    function updateEntities(dt) {
+        allEnemies.forEach(function (enemy) {
+            enemy.update(dt);
+        });
+        player.update();
+    }
+
+    function render() {
+
+        var rowImages = ['images/water-block.png', // Top row is water
+        'images/stone-block.png', // Row 1 of 3 of stone
+        'images/stone-block.png', // Row 2 of 3 of stone
+        'images/stone-block.png', // Row 3 of 3 of stone
+        'images/stone-block.png', // Row 1 of 2 of grass
+        'images/stone-block.png', // Row 2 of 2 of grass
+        'images/grass-block.png'],
+            numRows = 7,
+            numCols = 8,
+            row,
+            col;
+
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
+
+        for (row = 0; row < numRows; row++) {
+            for (col = 0; col < numCols; col++) {
+
+                ctx.drawImage(Resources.get(rowImages[row]), col * 101, row * 83);
+            }
+        }
+
+        renderEntities();
+    }
+
+    function renderEntities() {
+
+        allEnemies.forEach(function (enemy) {
+            enemy.render();
+        });
+
+        player.render();
+    }
+
+    function reset() {}
+
+    Resources.load(['images/stone-block.png', 'images/water-block.png', 'images/grass-block.png', 'images/enemy-bug.png', 'images/char-boy.png', 'images/Star.png']);
+    Resources.onReady(init);
+
+    global.ctx = ctx;
+}(undefined);
+"use strict";
+
+(function () {
+    var resourceCache = {};
+    var loading = [];
+    var readyCallbacks = [];
+
+    function load(urlOrArr) {
+        if (urlOrArr instanceof Array) {
+
+            urlOrArr.forEach(function (url) {
+                _load(url);
+            });
+        } else {
+
+            _load(urlOrArr);
+        }
+    }
+
+    function _load(url) {
+        if (resourceCache[url]) {
+
+            return resourceCache[url];
+        } else {
+
+            var img = new Image();
+            img.onload = function () {
+
+                resourceCache[url] = img;
+
+                if (isReady()) {
+                    readyCallbacks.forEach(function (func) {
+                        func();
+                    });
+                }
+            };
+
+            resourceCache[url] = false;
+            img.src = url;
+        }
+    }
+
+    function get(url) {
+        return resourceCache[url];
+    }
+
+    function isReady() {
+        var ready = true;
+        for (var k in resourceCache) {
+            if (resourceCache.hasOwnProperty(k) && !resourceCache[k]) {
+                ready = false;
+            }
+        }
+        return ready;
+    }
+
+    function onReady(func) {
+        readyCallbacks.push(func);
+    }
+
+    window.Resources = {
+        load: load,
+        get: get,
+        onReady: onReady,
+        isReady: isReady
+    };
+})();
