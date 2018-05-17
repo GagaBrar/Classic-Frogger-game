533cd67f3b5f24496257258f9268ddbed0a87d0b
Gagan Brar <brargagandeepsingh87@gmail.com>
2018-05-17 14:08:58 -0500

    images were minified

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 const autoprefixer = require('gulp-autoprefixer');
 const cleanCSS = require('gulp-clean-css');
+const imagemin = require('gulp-imagemin');
 
 gulp.task('css', function () {
             gulp.src(['./src/css/*.css'])
@@ -13,4 +14,10 @@ gulp.task('css', function () {
 
                 .pipe(gulp.dest('./dist/css/'))
 
+            });
+
+            gulp.task('images', function () {
+                gulp.src('./src/images/*')
+                    .pipe(imagemin())
+                    .pipe(gulp.dest('./dist/images'))
             });
