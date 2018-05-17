942a0e88a037c4adc6da4bfe870837d4c8a4bfe1
Gagan Brar <brargagandeepsingh87@gmail.com>
2018-05-17 14:32:24 -0500

    uglify is done

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,7 @@ const cleanCSS = require('gulp-clean-css');
 const imagemin = require('gulp-imagemin');
 const babel = require('gulp-babel');
 var concat = require('gulp-concat');
+var uglify = require('gulp-uglify');
 
 
 gulp.task('css', function () {
@@ -31,6 +32,7 @@ gulp.task('css', function () {
                         presets: ['env']
                     }))
                     .pipe(concat('main.js'))
+                    .pipe(uglify())
 
                     .pipe(gulp.dest('./dist/js/'))
             })
